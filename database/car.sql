39c13744b9c43bd4f8d31e6731ed9d1c9aaf7da8
Pramod Mahato <pramodkrmahato@hotmail.com>
2020-02-01 12:45:24 +0530

    SQL Updated file

--- database/car.sql
+++ database/car.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 11, 2018 at 02:57 PM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.2.1
+-- Generation Time: Feb 01, 2020 at 08:13 AM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `carrental`
+-- Database: `car`
 --
 
 -- --------------------------------------------------------
@@ -45,18 +45,9 @@ CREATE TABLE `cars` (
 --
 
 INSERT INTO `cars` (`car_id`, `car_name`, `car_nameplate`, `car_img`, `ac_price`, `non_ac_price`, `ac_price_per_day`, `non_ac_price_per_day`, `car_availability`) VALUES
-(1, 'Wagon R', 'KA19MG9910', 'assets/img/cars/wagon-r.png', 10, 8, 2000, 1600, 'yes'),
-(2, 'Alto 800', 'MH20GH9452', 'assets/img/cars/alto-800.png', 9, 7, 1800, 1400, 'yes'),
-(3, 'Innova', 'GA16NM9125', 'assets/img/cars/Innova.png', 13, 11, 2600, 2200, 'yes'),
-(4, 'Ford Figo', 'GJ17HZ4001', 'assets/img/cars/figo.png', 11, 9, 2200, 1800, 'yes'),
-(6, 'Mahindra Bolero', 'KL18MJ8472', 'assets/img/cars/bolero.jpg', 15, 13, 3000, 2600, 'yes'),
-(7, 'Honda Amaze', 'PJ16YX8820', 'assets/img/cars/amaze.png', 14, 12, 2800, 2400, 'no'),
-(8, 'Tata Nexon', 'WB15EX5005', 'assets/img/cars/nexon.jpg', 12, 10, 2400, 2000, 'yes'),
-(9, 'Swift Dzire', 'BR01HX8001', 'assets/img/cars/dzire.png', 10, 8, 2000, 1600, 'yes'),
-(10, 'Suzuki Ciaz', 'TN17MS1997', 'assets/img/cars/Suzuki_Ciaz_2017.jpg', 12, 10, 2400, 2000, 'yes'),
-(11, 'Mahindra XUV 500', 'KA12EX1883', 'assets/img/cars/Mahindra XUV.jpg', 15, 13, 3000, 2600, 'yes'),
-(12, 'Toyota Fortuner', 'GA08MX1997', 'assets/img/cars/Fortuner.png', 16, 14, 3200, 2800, 'yes'),
-(13, 'Suzuki Ertiga', 'MH02DC1997', 'assets/img/cars/maruti-suzuki-ertiga.jpg', 14, 12, 2800, 2400, 'yes');
+(14, 'Innova', 'PB 10 9999', 'assets/img/cars/innova.jpg', 30, 25, 3000, 2000, 'yes'),
+(15, 'Scorpio', 'PB 11 9988', 'assets/img/cars/scorpio.jpg', 30, 25, 2500, 1500, 'yes'),
+(16, 'Fortuner', 'PB 10 9987', 'assets/img/cars/fortuner.jpg', 30, 25, 4000, 3500, 'yes');
 
 -- --------------------------------------------------------
 
@@ -74,18 +65,9 @@ CREATE TABLE `clientcars` (
 --
 
 INSERT INTO `clientcars` (`car_id`, `client_username`) VALUES
-(1, 'nikhil'),
-(2, 'nikhil'),
-(3, 'nikhil'),
-(4, 'nikhil'),
-(10, 'nikhil'),
-(11, 'nikhil'),
-(6, 'roshan'),
-(7, 'roshan'),
-(8, 'roshan'),
-(9, 'roshan'),
-(12, 'roshan'),
-(13, 'roshan');
+(14, 'pramod'),
+(15, 'pramod'),
+(16, 'pramod');
 
 -- --------------------------------------------------------
 
@@ -107,8 +89,7 @@ CREATE TABLE `clients` (
 --
 
 INSERT INTO `clients` (`client_username`, `client_name`, `client_phone`, `client_email`, `client_address`, `client_password`) VALUES
-('nikhil', 'Nikhil Amin', '8547869542', 'nikhil@gmail.com', 'Mangalore', 'root'),
-('roshan', 'Roshan Raj', '8569874125', 'roshan@gmail.com', 'Surathkal', 'root');
+('pramod', 'Pramod Mahato', '9693814994', 'prainfosys@gmail.com', 'Ranchi', '123');
 
 -- --------------------------------------------------------
 
@@ -130,8 +111,7 @@ CREATE TABLE `customers` (
 --
 
 INSERT INTO `customers` (`customer_username`, `customer_name`, `customer_phone`, `customer_email`, `customer_address`, `customer_password`) VALUES
-('diwakar', 'Diwakar Vignesh', '9412578632', 'diwakar@gmail.com', 'Jalandhar', 'root'),
-('rakshith', 'Rakshith Kotian', '9541257862', 'rakshith@gmail.com', 'Surat', 'root');
+('client', 'Client Bhai', '9814072323', 'client@gmail.com', 'BH-1, 123', '123');
 
 -- --------------------------------------------------------
 
@@ -155,13 +135,7 @@ CREATE TABLE `driver` (
 --
 
 INSERT INTO `driver` (`driver_id`, `driver_name`, `dl_number`, `driver_phone`, `driver_address`, `driver_gender`, `client_username`, `driver_availability`) VALUES
-(1, 'Ravi Moolya', 'DL_TEH5974', '9547863157', 'Goa', 'Male', 'nikhil', 'yes'),
-(2, 'Nikitha Ghetia', 'DL_NDC7584', '9147523684', 'Gujarath', 'Female', 'nikhil', 'yes'),
-(3, 'S M Adil', 'DL_LSC8451', '9147523682', 'Kerala', 'Male', 'nikhil', 'yes'),
-(4, 'Sona Somshekar', 'DL_MND8415', '9187563240', 'Mandya', 'Female', 'roshan', 'no'),
-(5, 'Aditi Naik', 'DL_GAX9452', '7584960123', 'Vasco', 'Female', 'roshan', 'yes'),
-(6, 'Sudarshan Shetty', 'DL_YSH9542', '8421025476', 'Andra Pradesh', 'Male', 'roshan', 'yes'),
-(7, 'Jugal Chauhan', 'DL_XEC9742', '7541023695', 'Rajashthan', 'Male', 'roshan', 'yes');
+(8, 'Driver Bhai', 'DL1122323', '9693814994', 'Jalandhar', 'Male', 'pramod', 'yes');
 
 -- --------------------------------------------------------
 
@@ -210,18 +184,9 @@ CREATE TABLE `rentedcars` (
 --
 
 INSERT INTO `rentedcars` (`id`, `customer_username`, `car_id`, `driver_id`, `booking_date`, `rent_start_date`, `rent_end_date`, `car_return_date`, `fare`, `charge_type`, `distance`, `no_of_days`, `total_amount`, `return_status`) VALUES
-(574681245, 'diwakar', 4, 2, '2018-07-18', '2018-07-01', '2018-07-02', '2018-07-18', 11, 'km', 244, 1, 5884, 'R'),
-(574681246, 'diwakar', 6, 6, '2018-07-18', '2018-06-01', '2018-06-28', '2018-07-18', 15, 'km', 69, 27, 5035, 'R'),
-(574681247, 'diwakar', 3, 1, '2018-07-18', '2018-07-19', '2018-07-22', '2018-07-20', 13, 'km', 421, 3, 5473, 'R'),
-(574681248, 'rakshith', 1, 2, '2018-07-20', '2018-07-28', '2018-07-29', '2018-07-20', 10, 'km', 69, 1, 690, 'R'),
-(574681249, 'rakshith', 1, 2, '2018-07-23', '2018-07-24', '2018-07-25', '2018-07-23', 10, 'km', 500, 1, 5000, 'R'),
-(574681250, 'rakshith', 3, 2, '2018-07-23', '2018-07-23', '2018-07-24', '2018-07-23', 2600, 'days', NULL, 1, 2600, 'R'),
-(574681251, 'rakshith', 10, 1, '2018-07-23', '2018-07-25', '2018-07-30', '2018-07-23', 10, 'km', 60, 2, 600, 'R'),
-(574681252, 'rakshith', 11, 2, '2018-07-23', '2018-07-23', '2018-07-23', '2018-07-23', 13, 'km', 200, 0, 2600, 'R'),
-(574681253, 'diwakar', 6, 7, '2018-07-23', '2018-07-23', '2018-08-03', '2018-07-23', 2600, 'days', NULL, 11, 28600, 'R'),
-(574681254, 'diwakar', 12, 5, '2018-07-23', '2018-07-23', '2018-07-26', '2018-07-23', 3200, 'days', NULL, 3, 9600, 'R'),
-(574681255, 'rakshith', 8, 5, '2018-07-23', '2018-07-23', '2018-08-08', '2018-07-23', 2400, 'days', NULL, 16, 38400, 'R'),
-(574681257, 'rakshith', 7, 4, '2018-08-11', '2018-08-13', '2018-08-17', NULL, 14, 'km', NULL, NULL, NULL, 'NR');
+(574681259, 'client', 14, 8, '2020-01-31', '2020-02-08', '2020-02-09', '2020-01-31', 3000, 'days', NULL, 1, 3000, 'R'),
+(574681260, 'client', 15, 8, '2020-01-31', '2020-02-22', '2020-02-24', '2020-01-31', 30, 'km', 200, 2, 6000, 'R'),
+(574681261, 'client', 16, 8, '2020-02-01', '2020-02-07', '2020-02-09', '2020-02-01', 30, 'km', 200, 2, 6000, 'R');
 
 --
 -- Indexes for dumped tables
@@ -278,19 +243,19 @@ ALTER TABLE `rentedcars`
 -- AUTO_INCREMENT for table `cars`
 --
 ALTER TABLE `cars`
-  MODIFY `car_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `car_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `driver`
 --
 ALTER TABLE `driver`
-  MODIFY `driver_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `driver_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `rentedcars`
 --
 ALTER TABLE `rentedcars`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=574681258;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=574681262;
 
 --
 -- Constraints for dumped tables
